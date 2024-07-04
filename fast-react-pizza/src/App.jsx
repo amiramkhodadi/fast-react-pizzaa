cfdd51e57f258bd28c54191f0d45a204588b8313
amiramkhodadi <135073823+amiramkhodadi@users.noreply.github.com>
2024-07-04 01:53:07 -0700

    create App layout

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -4,27 +4,33 @@ import Menu from "./feature/menu/Menu";
 import Cart from "./feature/cart/Cart";
 import CreateOrder from "./feature/order/CreateOrder";
 import Order from "./feature/order/Order";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "/order/new",
-    element: <CreateOrder />,
-  },
-  {
-    path: "/order/:orderid",
-    element: <Order />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderid",
+        element: <Order />,
+      },
+    ],
   },
 ]);
 
---
+++ fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <div>
+      <Link to="/"> Fast React Co.</Link>
+      <p>Amir</p>
+    </div>
+  );
+}
+
+export default Header;
